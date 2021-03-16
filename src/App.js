daf1815a22e031d2fd732044a794a596453ad559
Diego-MV <diegomoralesval@gmail.com>
2021-03-16 10:54:33 -0300

    add switch selector and calculator

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+import { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import styled from '@emotion/styled';
 
 import Calculator from './pages/Calculator'
 import Selector from './pages/Selector'
 
+const Label = styled.label`
+  font-family: "Bebas Neue", cursivel;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 1.5rem;
+  display: block;
+`;
+
 function App() {
+  const [side, setside] = useState(false);
+  const switchSide = () => {
+    setside(!side);
+    console.log(side)
+  }
+
   return (
-    <BrowserRouter>
-      <Switch>
-      <Route exact path="/KGplusLB" component={Calculator}/>
-        <Route exact path="/" component={Calculator}/>
-        <Route exact path="/Selector" component={Selector}/>
-      </Switch>
-    </BrowserRouter>
+    <div>
+      {side ? <Selector/> : <Calculator/>}
+      <div class="form-check form-switch">
+        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onChange={switchSide}/>
+        {side ? <Label class="form-check-label" for="flexSwitchCheckDefault">Cambiar a calculadora</Label> : <Label class="form-check-label" for="flexSwitchCheckDefault">Cambiar a Selector</Label>}
+      </div>
+    </div>
   );
 }
 
